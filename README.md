c85a7779855428d5e570cb0278235b0dd6147687
helenowitz <31396759+helenowitz@users.noreply.github.com>
2024-06-27 23:02:20 -0400

    Updated README and added affirmations to script

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 Hello Git and Github
+
+Here we have a practice project from Codecademy.
+- The object of this project is to create a random message/joke/astrology/nonsense generator. Utilizing three (3) or more parts of 'random' content generate a new message every time the application is opened.
+
+I have chosen to go with an Affirmation Generator. Every morning you can open the app and recieve your daily affirmation for you to keep and repeat throughout the day.
+
+
+ [ -- SOURCES --]
+ https://www.verywellmind.com/positive-daily-affirmations-7097067
+ 
---
+++ dailyAffirmations.js
@@ -0,0 +1,29 @@
+const affirmations = [
+    "I am a strong, capable person",
+    "I have done difficult things in the past, and I can do them again",
+    "I have experienced challenges in the past, and I am more resilient because of this",
+    "I am allowed to feel upset, angry, and sad sometimes—that’s part of being human",
+    "My personal boundaries are important, and I’m allowed to express my needs to others",
+    "'No' is a complete sentence, and I don’t have to explain or justify my boundaries",
+    "I am allowed to feel good and to experience pleasure in life",
+    "I am worthy of receiving good things and of accomplishing my goals in life",
+    "The past is the past, and my past doesn’t predict my future",
+    "I forgive myself for the mistakes I made, and I refuse to hold them against myself",
+    "I am allowed to take up space, to have desires, and to have a voice",
+    "I don’t have to give up my hopes and dreams",
+    "All this hard work I am putting into achieving my goals will pay off",
+    "I am capable of making healthy choices",
+    "I know my worth",
+    "I deserve to be loved and to love others",
+    "Growth is sometimes bumpy and isn’t always linear, but I will stay the course",
+    "Healing is within reach for me",
+    "I love my body, my mind, my dreams, and my goals",
+    "Negative thoughts do not serve me anymore",
+    "I will surround myself with people who love and support me unconditionally",
+    "I accept myself for who I am",
+    "With positive thoughts and self-confidence, I will be unstoppable",
+    "I am proud of myself and will continue to strive to do well",
+    "Today, I will do my best"
+]
+
+console.log(`${affirmations[Math.floor(Math.random()*affirmations.length)]}\n${affirmations[Math.floor(Math.random()*affirmations.length)]}\n${affirmations[Math.floor(Math.random()*affirmations.length)]}`)
